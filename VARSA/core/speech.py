aca93f076afb0bd57a693a5a083efee8b7fa913f
Ketaki Joshi <joshiketaki101@gmail.com>
2025-06-05 18:52:33 +0530

    initial commit

---
+++ VARSA/core/speech.py
@@ -0,0 +1,27 @@
+import speech_recognition as sr
+import pyttsx3
+
+engine=pyttsx3.init()
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()  #listens while being spoken to
+
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("VARSA listening...")
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+    try:
+        command = r.recognize_google(audio)
+        print(f"You said: {command}")
+        return command.lower()
+    except sr.UnknownValueError:
+        print("Sorry, I didn't catch that.")
+        speak("Sorry, I didn't catch that.")
+        return ""
+    except sr.RequestError:
+        print("Speech service unavailable.")
+        speak("Speech service is unavailable.")
+        return ""
