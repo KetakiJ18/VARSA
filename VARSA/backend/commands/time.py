8a5a17fe2a6284196b4312c0535f330f06ce02c0
Ketaki Joshi <joshiketaki101@gmail.com>
2025-06-10 19:34:22 +0530

    Delete VARSA/backend directory

--- VARSA/backend/commands/time.py
+++
@@ -1,6 +0,0 @@
-import time
-from core.speech import speak
-def set_reminder(reminder, seconds):
-    speak(f"Setting a reminder for {reminder} in {seconds} seconds.")
-    time.sleep(seconds)
-    speak(f"Reminder: {reminder}")
